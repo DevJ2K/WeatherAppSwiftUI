278b23d68f5bf1db2472ad2a62d3a4e727c8d673
theo <ajavontheopro@gmail.com>
2024-04-08 14:24:20 +0200

    handling no city found

--- weatherfinal_proj/weatherfinal_proj/ContentView.swift
+++ weatherfinal_proj/weatherfinal_proj/ContentView.swift
@@ -13,9 +13,13 @@ import SwiftUI
 struct ContentView: View {
     @State private var cities = [City]()
     @State private var searchText = ""
+    
+    // Search bar state
     @State private var showSearchBar = false
-    @State private var noAnimationDisplay = false
+    @State private var searchBarNoAnimation = false
+    @State private var noSearchMatchingAnimation = false
     @FocusState private var focusedSearch: Bool?
+    
     @State private var selectedTab: Tab = .currently
     @Environment (\.colorScheme) var colorScheme
     @ObservedObject var locationManager = LocationManager.shared
@@ -43,7 +47,7 @@ struct ContentView: View {
         hideKeyboard()
         // Wait for the keyboard to hide before performing animation
         //        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-        noAnimationDisplay = false
+        searchBarNoAnimation = false
         withAnimation(.easeInOut(duration: 0.2)) {
             showSearchBar = false
         }
@@ -72,6 +76,11 @@ struct ContentView: View {
                             .focused($focusedSearch, equals: true)
                             .onSubmit {
                                 hideKeyboard()
+                            }
+                            .onChange(of: focusedSearch) { newValue in
+                                withAnimation(.easeInOut(duration: 0.2)) {
+                                    noSearchMatchingAnimation = newValue ?? false
+                                }
                             }
                             .onChange(of: searchText) { newValue in
                                 if (searchText.isEmpty == false) {
@@ -110,7 +119,7 @@ struct ContentView: View {
                         
                         HStack {
                             Button(action: {
-                                noAnimationDisplay = true
+                                searchBarNoAnimation = true
                                 withAnimation(.easeInOut(duration: 0.2)) {
                                     searchText = ""
                                     showSearchBar = true
@@ -131,7 +140,6 @@ struct ContentView: View {
                                         locationManager.cityInfo = cityInfoFetching
                                     }
                                 } else {
-                                    
                                     print("Geolocation pressed but cityLocation not exist !")
                                 }
                             }, label: {
@@ -150,71 +158,68 @@ struct ContentView: View {
             .padding(.top, 15)
             .padding(.horizontal)
             .padding(.bottom, 12)
-            .background(noAnimationDisplay ? Color.primary.opacity(colorScheme == .dark ? 0.1 : 0.25) : Color.clear)
+            .background(searchBarNoAnimation ? Color.primary.opacity(colorScheme == .dark ? 0.1 : 0.25) : Color.clear)
             
-            
-            if (noAnimationDisplay) {
-                if (searchText.isEmpty || searchText.count == 1) {
-                    VStack {
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(colorScheme == .dark ? .black : .white)
-                    .ignoresSafeArea(.all)
-                } else if (cities.count == 0 && searchText.count > 1 && LocationManager.shared.isFetchingCity == false) {
-                    VStack(spacing: 12) {
-                        Spacer()
-                        Image(systemName: "magnifyingglass")
-                            .foregroundStyle(.gray)
-                            .font(.system(size: 48))
-                        VStack(spacing: 2) {
-                            Text("**No Results**")
-                                .font(.title2)
-                            Text("No results found for \"\(searchText)\".")
+            if (searchBarNoAnimation) {
+                GeometryReader(content: { geometry in
+                    if (searchText.isEmpty || searchText.count == 1) {
+                        VStack {
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(colorScheme == .dark ? .black : .white)
+                    } else if (cities.count == 0 && searchText.count > 1) {
+                        VStack(spacing: 12) {
+                            Spacer()
+                            Image(systemName: "magnifyingglass")
                                 .foregroundStyle(.gray)
-                                .font(.subheadline)
+                                .font(.system(size: 48))
+                            VStack(spacing: 2) {
+                                Text("**No Results**")
+                                    .font(.title2)
+                                Text("No results found for \"\(searchText)\".")
+                                    .foregroundStyle(.gray)
+                                    .font(.subheadline)
+                            }
+                            Spacer()
                         }
-                        Spacer()
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(colorScheme == .dark ? .black : .white)
-                } else {
-                    List(cities, id: \.id) { city in
-                        Button(action: {
-                            print("\(city.id) : \(city.longitude) | \(city.latitude)")
-                            searchText = ""
-                            
-                            locationManager.cityLocation = city
-                            if (locationManager.cityLocation != nil) {
-                                Task {
-                                    let cityInfoFetching = await fetchCityInfo(city: locationManager.cityLocation!)
-                                    locationManager.cityInfo = cityInfoFetching
-//                                    DispatchQueue.main.async {
-//                                        self.cityInfo = cityInfoFetching
-//                                    }
+                        .frame(width: geometry.size.width, height: (noSearchMatchingAnimation) ? geometry.size.height / 2 : geometry.size.height)
+                        .background(colorScheme == .dark ? .black : .white)
+                    } else {
+                        List(cities, id: \.id) { city in
+                            Button(action: {
+                                print("\(city.id) : \(city.longitude) | \(city.latitude)")
+                                searchText = ""
+                                
+                                locationManager.cityLocation = city
+                                if (locationManager.cityLocation != nil) {
+                                    Task {
+                                        let cityInfoFetching = await fetchCityInfo(city: locationManager.cityLocation!)
+                                        locationManager.cityInfo = cityInfoFetching
+                                    }
+                                    
+                                } else {
+                                    print("Invalid City")
                                 }
-                                
-                            } else {
-                                print("Invalid City")
+                                focusedSearch = false
+                                hideKeyboard()
+                                searchBarNoAnimation = false
+                                withAnimation(.easeInOut(duration: 0.2)) {
+                                    showSearchBar = false
+                                }
+                            }) {
+                                HStack(spacing: 0) {
+                                    Image(systemName: "building.2")
+                                        .padding(.trailing, 16)
+                                    Text("**\(city.name)**")
+                                    + Text((city.admin1 != nil) ? " \(city.admin1!)" : "")
+                                    + Text((city.country != nil) ? ", \(city.country!)" : "")
+                                    
+                                }
                             }
-                            focusedSearch = false
-                            hideKeyboard()
-                            noAnimationDisplay = false
-                            withAnimation(.easeInOut(duration: 0.2)) {
-                                showSearchBar = false
-                            }
-                        }) {
-                            HStack(spacing: 0) {
-                                Image(systemName: "building.2")
-                                    .padding(.trailing, 16)
-                                Text("**\(city.name)**")
-                                + Text((city.admin1 != nil) ? " \(city.admin1!)" : "")
-                                + Text((city.country != nil) ? ", \(city.country!)" : "")
-                                
-                            }
+                            .foregroundStyle(.primary)
                         }
-                        .foregroundStyle(.primary)
                     }
-                }
+                })
             } else {
                 TabView(selection: $selectedTab) {
                     if (locationManager.cityLocation != nil && locationManager.cityInfo != nil) {
@@ -300,20 +305,20 @@ struct ContentView: View {
                 }
                 .animation(nil, value: selectedTab)
                 .searchable(text: $searchText)
-                //                .overlay(alignment: .bottom) {
-                //                    MyAppBar(selectedTab: $selectedTab)
-                //                }
+                .overlay(alignment: .bottom) {
+                    MyAppBar(selectedTab: $selectedTab)
+                }
                 .tabViewStyle(.page(indexDisplayMode: .never))
             }
-            MyAppBar(selectedTab: $selectedTab)
-            //                .ignoresSafeArea(.all)
+            
+            //            MyAppBar(selectedTab: $selectedTab)
         }
         .padding(.bottom, 20)
         .background(showSearchBar ?
                     LinearGradient(colors: [colorScheme == .dark ? .black : .white], startPoint: .center, endPoint: .center) :
                         LinearGradient(
                             gradient: Gradient(colors: [.purple.opacity(colorScheme == .dark ? 0.2 : 0.7), .indigo.opacity(0.8)]), startPoint: .top, endPoint: .bottom))
-        //        .ignoresSafeArea(edges: noAnimationDisplay ? [] : .bottom)
+        //        .ignoresSafeArea(edges: searchBarNoAnimation ? [] : .bottom)
         .ignoresSafeArea(edges: .bottom)
     }
 }
