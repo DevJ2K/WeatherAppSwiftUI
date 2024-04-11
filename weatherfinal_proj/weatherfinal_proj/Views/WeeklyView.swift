a2a7ffff909466ed3d376e335a547ef757432b59
theo <ajavontheopro@gmail.com>
2024-04-11 18:02:07 +0200

    Weekly tab view done

--- weatherfinal_proj/weatherfinal_proj/Views/WeeklyView.swift
+++ weatherfinal_proj/weatherfinal_proj/Views/WeeklyView.swift
@@ -11,42 +11,60 @@ struct WeeklyView: View {
     var cityInfo: CityInfo?
     
     var body: some View {
-        VStack {
-            Text("Weekly")
-                .font(.title)
-                .bold()
-            if (cityInfo?.city != nil && cityInfo?.current != nil) {
-                Text(cityInfo?.city?.name ?? "Unknown")
-                    .font(.title2)
-                Text(cityInfo?.city?.admin1 ?? "Unknown")
-                    .font(.title2)
-                Text(cityInfo?.city?.country ?? "Unknown")
-                    .font(.title2)
-                Text("\(cityInfo?.city?.latitude ?? 0) | \(cityInfo?.city?.longitude ?? 0)")
-                if (cityInfo?.daily != nil) {
-                    ForEach(0 ..< cityInfo!.daily!.time.count, id: \.self) { i in
-                        HStack {
-                            Text("\(cityInfo!.daily!.time[i].prefix(10))")
-                            Spacer()
-                            Text("\(String(format: "%.1f", cityInfo!.daily!.temperature_2m_min[i]))°C")
-                            Text("\(String(format: "%.1f", cityInfo!.daily!.temperature_2m_max[i]))°C")
-//                            Spacer()
-                            Text("\(getWeatherDescription(weather_code: cityInfo!.daily!.weather_code[i])?.dayDescription ?? "")")
-                            Spacer()
-//                                .multilineTextAlignment(.center)
-                        }
-//                        Text("\(value)")
-                    }
-                }
-            } else {
-                Text("Invalid data")
+        ScrollView {
+            VStack {
+                WeeklyChart(weekly: cityInfo?.daily)
+                ScrollViewContent()
             }
         }
-        .padding()
+    }
+    
+    @ViewBuilder
+    func ScrollViewContent() -> some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: 0) {
+                if (cityInfo?.daily != nil) {
+                    ForEach(0 ..< min(7, cityInfo!.daily!.time.count), id: \.self) { i in
+                        VStack(spacing: 10) {
+                            Text("\(cityInfo!.daily!.time[i].split(separator: "-")[2])/\(cityInfo!.daily!.time[i].split(separator: "-")[1])")
+                                .font(.system(size: 14, weight: .light))
+                                .opacity(0.8)
+//                            Text("\(getWeatherDescription(weather_code: cityInfo!.hourly!.weather_code[i])?.dayDescription ?? "")")
+                            MiniSceneView(sceneName: "cloudy_night.scn")
+                                .frame(height: 40)
+                            VStack(spacing: 6) {
+                                HStack(spacing: 4) {
+//                                    Image(systemName: getTempLogo(temperature: cityInfo!.hourly!.temperature_2m[i]))
+//                                        .font(.system(size: 14))
+//                                        .opacity(0.8)
+                                    Text("\(String(format: "%.1f", cityInfo!.hourly!.temperature_2m[i]))°C")
+                                        .font(.system(size: 14, weight: .light))
+                                        .opacity(0.8)
+                                }
+                                HStack(spacing: 4) {
+                                    Image(systemName: "wind")
+                                        .font(.system(size: 14))
+                                        .opacity(0.8)
+                                    Text("\(String(format: "%.1f", cityInfo!.hourly!.wind_speed_10m[i]))km/h")
+                                        .font(.system(size: 14, weight: .light))
+                                        .opacity(0.8)
+                                }
+                            }
+                        }
+                        .padding()
+//                        .background(.blue.gradient)
+                    }
+                }
+            }
+        }
+        .background(.black.opacity(0.2))
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+        .padding(.horizontal)
     }
 }
 
 #Preview {
+    return ContentView()
     let jsonString = """
         {
             "city": {
@@ -67,17 +85,23 @@ struct WeeklyView: View {
                 "wind_speed_10m": 18.9
             },
             "hourly": {
-                "time": ["2024-04-04T00:00", "2024-04-04T01:00", "2024-04-04T02:00", "2024-04-04T03:00"],
-                "temperature_2m": [13.0, 12.8, 13.3, 13.2],
-                "weather_code": [2, 61, 61, 61],
-                "wind_speed_10m": [18.2, 17.3, 18.7, 17.2]
+                "time": ["2024-04-04T00:00", "2024-04-04T01:00", "2024-04-04T02:00", "2024-04-04T03:00", "2024-04-04T04:00", "2024-04-04T05:00", "2024-04-04T06:00", "2024-04-04T07:00", "2024-04-04T08:00", "2024-04-04T09:00",
+                                  "2024-04-04T10:00", "2024-04-04T11:00", "2024-04-04T12:00", "2024-04-04T13:00", "2024-04-04T14:00",
+                                  "2024-04-04T15:00", "2024-04-04T16:00", "2024-04-04T17:00", "2024-04-04T18:00", "2024-04-04T19:00",
+                                  "2024-04-04T20:00", "2024-04-04T21:00", "2024-04-04T22:00", "2024-04-04T23:00",],
+                "temperature_2m": [13.0, 7.8, 13.3, 13.2, 25, 26.7, 28.1, 29.5, 30.2, 31.8, 32.5, 32.9, 33.2, 33.5, 33.8, 33.9, 33.6, 33.2, 32.8, 32.5, 32.2,
+                                                                                                                                    32.0, 31.8, 31.5, 31.2, 30.9, 30.6, 30.3, 30.0],
+                "weather_code": [2, 61, 61, 61, 61, 61, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32],
+                "wind_speed_10m": [18.2, 17.3, 18.7, 17.2, 19, 20.1, 21.5, 22.3, 23.6, 24.9, 25.3, 25.8, 26.2, 26.5, 26.7, 26.8, 26.6, 26.3, 26.0, 25.7, 25.4,
+                                                                                                                                                                                                                                                                                                                                         25.1, 24.8, 24.5, 24.2, 23.9, 23.6, 23.3, 23.0]
             },
             "daily": {
-                "time": ["2024-04-04", "2024-04-05", "2024-04-06"],
-                "weather_code": [80, 61, 3],
-                "temperature_2m_max": [18.4, 17.2, 24.9],
-                "temperature_2m_min": [12.0, 13.0, 12.1]
+                "time": ["2024-04-04", "2024-04-05", "2024-04-06", "2024-04-07", "2024-04-08", "2024-04-09", "2024-04-10"],
+                "weather_code": [80, 61, 3, 45, 80, 45, 45],
+                "temperature_2m_max": [18.4, 17.2, 24.9, 16.4, 14.6, 17.7, 29.0],
+                "temperature_2m_min": [12.0, 13.0, 12.1, 12, 5.0, -3, 23.8]
             }
+        
         }
         """
     guard let jsonData = jsonString.data(using: .utf8) else {
@@ -89,10 +113,11 @@ struct WeeklyView: View {
         // Attempt JSON decoding
         let cityInfo = try JSONDecoder().decode(CityInfo.self, from: jsonData)
         return WeeklyView(cityInfo: cityInfo)
+            .background(LinearGradient(
+                gradient: Gradient(colors: [.purple.opacity(0.2), .indigo.opacity(0.8)]), startPoint: .top, endPoint: .bottom))
     } catch {
         // Handle decoding error
         print("Error decoding JSON:", error)
         return Text("Invalid previews")
     }
-//    WeeklyView(searchLocation: "Paris")
 }
--- weatherfinal_proj/weatherfinal_proj/Widgets/WeeklyChart.swift
+++ weatherfinal_proj/weatherfinal_proj/Widgets/WeeklyChart.swift
@@ -19,74 +19,90 @@ import Charts
 struct ChartDayData {
     var id = UUID()
     var time: String
-    //    var hour: Int
     var timeDate: Date
-    var temperature: Double
-    var weather_code: Int8
-    var wind_speed: Double
+    var temperature_2m_min: Double
+    var temperature_2m_max: Double
 }
 
-// Function to convert time strings to Hour objects
-//func convertToHours(timeString: String) -> Date {
-//    let dateFormatter = DateFormatter()
-//    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm"
-//    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-//    dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-//    
-//    //    print(timeString)
-//    let date = dateFormatter.date(from: timeString)!
-//    //    print(date)
-//    //    print("++++")
-//    return date
-//}
+func convertToDate(timeString: String) -> Date {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+    dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+    
+    //    print(timeString)
+    let date = dateFormatter.date(from: timeString)!
+    //    print(date)
+    //    print("++++")
+    return date
+}
 
-func dateToChartsData(weekly: WeeklyData?) -> [ChartDayData] {
+func weeklyToChartsData(weekly: WeeklyData?) -> [ChartDayData] {
     var chartDataArray: [ChartDayData] = []
     
     
     //    ForEach(0 ..< min(24, hourly!.time.count), id: \.self) { i in
-    for i in 0 ..< min(24, hourly!.time.count) {
-        let chartData = ChartHourData(time: hourly!.time[i], timeDate: convertToHours(timeString: hourly!.time[i]), temperature: hourly!.temperature_2m[i], weather_code: hourly!.weather_code[i], wind_speed: hourly!.wind_speed_10m[i])
+    for i in 0 ..< min(24, weekly!.time.count) {
         
+        let chartData = ChartDayData(time: weekly!.time[i], timeDate: convertToDate(timeString: weekly!.time[i]), temperature_2m_min: weekly!.temperature_2m_min[i], temperature_2m_max: weekly!.temperature_2m_max[i])
         chartDataArray.append(chartData)
     }
     //    print(chartDataArray)
     return chartDataArray
 }
 
-struct TodayChart: View {
+struct WeeklyChart: View {
     
-    var hourly: HourlyData?
-    @State private var rawSelectedTemperature: ClosedRange<Date>?
+    var weekly: WeeklyData?
+
     var body: some View {
-        let chartsData: [ChartHourData] = hourlyToChartsData(hourly: self.hourly)
+        let chartsData: [ChartDayData] = weeklyToChartsData(weekly: self.weekly)
+        let stackColors: [Color] = [.blue, .orange]
         
         VStack(spacing: 2) {
-            Text("Today temperatures")
+            Text("Weekly temperatures")
                 .font(.system(size: 16, weight: .light, design: .rounded))
                 .opacity(0.7)
                 .padding(.top)
             Chart {
-                ForEach(chartsData, id: \.id) { chartHour in
+                ForEach(chartsData, id: \.id) { chartDay in
                     LineMark(
-                        x: .value("Hour", chartHour.timeDate),
-                        y: .value("Temperature", chartHour.temperature)
+                        x: .value("Day", chartDay.timeDate, unit: .day),
+                        y: .value("Max", chartDay.temperature_2m_max),
+                        series: .value("max", "Max temperature")
                     )
+                    .lineStyle(StrokeStyle(lineWidth: 1))
                     .interpolationMethod(.catmullRom)
-                    .foregroundStyle(Color.blue.gradient)
+//                    .foregroundStyle(Color.orange.gradient)
+                    .foregroundStyle(by: .value("max", "Max temperature"))
+                    
                     .symbol {
                         Circle()
-                            .fill(.indigo)
-                            .frame(width: 7)
+                            .fill(.white)
+                            .frame(width: 4)
+                    }
+                    LineMark(
+                        x: .value("Day", chartDay.timeDate, unit: .day),
+                        y: .value("Min", chartDay.temperature_2m_min),
+                        series: .value("min", "Min temperature")
+                    )
+                    .lineStyle(StrokeStyle(lineWidth: 1))
+                    .interpolationMethod(.catmullRom)
+//                    .foregroundStyle(Color.blue.gradient)
+                    .foregroundStyle(by: .value("min", "Min temperature"))
+                    .symbol {
+                        Circle()
+                            .fill(.white)
+                            .frame(width: 4)
                     }
                     
-                    AreaMark(
-                        x: .value("Hour", chartHour.timeDate),
-                        y: .value("Temperature", chartHour.temperature)
-                    )
-                    .interpolationMethod(.catmullRom)
-                    .foregroundStyle(LinearGradient(colors: [.blue, .blue.opacity(0.7), .blue.opacity(0.05)], startPoint: .top, endPoint: .bottom))
-                    .opacity(0.2)
+                    //                    AreaMark(
+                    //                        x: .value("Hour", chartHour.timeDate),
+                    //                        y: .value("Temperature", chartHour.temperature)
+                    //                    )
+                    //                    .interpolationMethod(.catmullRom)
+                    //                    .foregroundStyle(LinearGradient(colors: [.blue, .blue.opacity(0.7), .blue.opacity(0.05)], startPoint: .top, endPoint: .bottom))
+                    //                    .opacity(0.2)
                     
                     
                     
@@ -94,23 +110,44 @@ struct TodayChart: View {
                 
             }
             .frame(height: 250)
+            .chartLegend(position: .bottom, alignment: .leading, spacing: 24) {
+                HStack(spacing: 12) {
+                    HStack(spacing: 2) {
+                        Circle()
+                            .fill(.blue)
+                            .frame(width: 6)
+                        Text("Min temperature")
+                            .font(.caption2)
+                            .foregroundStyle(.white).opacity(0.4)
+                    }
+                    HStack(spacing: 2) {
+                
+                        Circle()
+                            .fill(.red)
+                            .frame(width: 6)
+                        Text("Min temperature")
+                            .font(.caption2)
+                            .foregroundStyle(.white).opacity(0.4)
+                    }
+                    
+                }
+            }
             .chartXAxis {
-                AxisMarks(preset: .aligned, values: .stride(by: .hour, count: 3)) { axisValue in
+                AxisMarks(values: .stride(by: .day, count: 1)) { axisValue in
                     if let date = axisValue.as(Date.self) {
-                        AxisValueLabel() {
+                        AxisValueLabel(centered: true, multiLabelAlignment: .center) {
                             let formatter = DateFormatter()
-                            formatter.dateFormat = "HH:mm"
+                            formatter.dateFormat = "MM/dd"
                             formatter.locale = Locale(identifier: "en_US_POSIX")
                             formatter.timeZone = TimeZone(secondsFromGMT: 0)
                             let formattedDate = formatter.string(from: date)
                             //                            print(axisValue.index)
-                            if (axisValue.index == 8) {
+                            if (axisValue.index == 7) {
                                 return Text("")
                                     .font(.system(size: 8))
+                                
                             }
-                            return Text("\(formattedDate)")
-                            //                                .foregroundColor(Color.white)
-                                .font(.system(size: 8))
+                            return Text("\(formattedDate)").font(.system(size: 8))
                         }
                     }
                     AxisTick(stroke: StrokeStyle(lineWidth: 0))
@@ -171,11 +208,12 @@ struct TodayChart: View {
                                                                                                                                                                                                                                                                                                                                          25.1, 24.8, 24.5, 24.2, 23.9, 23.6, 23.3, 23.0]
             },
             "daily": {
-                "time": ["2024-04-04", "2024-04-05", "2024-04-06"],
-                "weather_code": [80, 61, 3],
-                "temperature_2m_max": [18.4, 17.2, 24.9],
-                "temperature_2m_min": [12.0, 13.0, 12.1]
+                "time": ["2024-04-04", "2024-04-05", "2024-04-06", "2024-04-07", "2024-04-08", "2024-04-09", "2024-04-10"],
+                "weather_code": [80, 61, 3, 45, 80, 45, 45],
+                "temperature_2m_max": [18.4, 17.2, 24.9, 16.4, 14.6, 17.7, 29.0],
+                "temperature_2m_min": [12.0, 13.0, 12.1, 12, 5.0, -3, 23.8]
             }
+        
         }
         """
     guard let jsonData = jsonString.data(using: .utf8) else {
@@ -186,7 +224,7 @@ struct TodayChart: View {
     do {
         // Attempt JSON decoding
         let cityInfo = try JSONDecoder().decode(CityInfo.self, from: jsonData)
-        return TodayView(cityInfo: cityInfo)
+        return WeeklyView(cityInfo: cityInfo)
             .background(LinearGradient(
                 gradient: Gradient(colors: [.purple.opacity(0.2), .indigo.opacity(0.8)]), startPoint: .top, endPoint: .bottom))
     } catch {
@@ -194,14 +232,4 @@ struct TodayChart: View {
         print("Error decoding JSON:", error)
         return Text("Invalid previews")
     }
-    
-    
-    
-    TodayChart(
-        hourly: HourlyData(time: ["2024-04-04T00:00", "2024-04-04T01:00", "2024-04-04T02:00", "2024-04-04T03:00", "2024-04-04T04:00", "2024-04-04T05:00", "2024-04-04T06:00", "2024-04-04T07:00", "2024-04-04T08:00", "2024-04-04T09:00",
-                                  "2024-04-04T10:00", "2024-04-04T11:00", "2024-04-04T12:00", "2024-04-04T13:00", "2024-04-04T14:00",
-                                  "2024-04-04T15:00", "2024-04-04T16:00", "2024-04-04T17:00", "2024-04-04T18:00", "2024-04-04T19:00",
-                                  "2024-04-04T20:00", "2024-04-04T21:00", "2024-04-04T22:00", "2024-04-04T23:00",], temperature_2m: [13.0, 7.8, 13.3, 13.2, 25, 26.7, 28.1, 29.5, 30.2, 31.8, 32.5, 32.9, 33.2, 33.5, 33.8, 33.9, 33.6, 33.2, 32.8, 32.5, 32.2,
-                                                                                                                                     32.0, 31.8, 31.5, 31.2, 30.9, 30.6, 30.3, 30.0], weather_code: [2, 61, 61, 61, 61, 61, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32], wind_speed_10m: [18.2, 17.3, 18.7, 17.2, 19, 20.1, 21.5, 22.3, 23.6, 24.9, 25.3, 25.8, 26.2, 26.5, 26.7, 26.8, 26.6, 26.3, 26.0, 25.7, 25.4,
-                                                                                                                                                                                                                                                                                                                                          25.1, 24.8, 24.5, 24.2, 23.9, 23.6, 23.3, 23.0]))
 }
